d00cd03a707ccdd96e19cf4e9e3fb008285d5c4e
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-08-23 08:38:54 -0600

    created user document

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: () => Promise.resolve(false),
+            message: 'Email validation failed'
+          }      
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought',
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+    }
+  );
+
+userSchema.virtual('friendCount')
+// Getter
+.get(function () {
+  return this.friends.length;
+});
+  
+const User = model('user', userSchema);
+
+module.exports = User;
